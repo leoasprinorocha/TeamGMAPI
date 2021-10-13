4a906e4faf30d3924964388bc33bc1d587559c07
Leonardo Rocha <leonardo@eosconsultores.com.br>
2021-10-12 23:23:06 -0400

    Add project files.

---
+++ TeamGMAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamGMAPI.Controllers
+{
+    public class AuthenticationController : ControllerBase
+    {
+       
+        [Route("api/[controller]")]
+        [ApiController]
+
+        
+    }
+}

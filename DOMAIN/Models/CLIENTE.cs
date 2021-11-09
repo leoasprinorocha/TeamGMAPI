7491c2fc549eaa6fda2132e1426bc39c5586ea0a
Leonardo Rocha <leonardo@eosconsultores.com.br>
2021-11-08 21:15:57 -0400

    Iniciando model cliente

---
+++ DOMAIN/Models/CLIENTE.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.DOMAIN.Models
+{
+     public class CLIENTE
+    {
+        public int ID_CLIENTE { get; set; }
+        public string NOME { get; set; }
+    }
+}
